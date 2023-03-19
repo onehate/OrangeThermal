065e831a9c89d3ade0d1cde33270ae3638e538cc
onehate <onehate@gmail.com>
2023-03-19 21:39:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -96,6 +96,7 @@ must_hit_temp = False
 # Only used if must_hit_temp is True
 cone_slope_adj = 0.4
 
+tune_cycles = 3
 tune_target_temp = 150
 
 ########################################################################
