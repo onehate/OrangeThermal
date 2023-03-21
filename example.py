bd55969fdfc3e1b689b52073d8b7e29785751f59
onehate <onehate@gmail.com>
2023-03-21 22:52:06 +0300

    kkk

--- example.py
+++ example.py
@@ -22,8 +22,6 @@ def main():
     """
     Example demonstrating continous read out of temperature of MAX31865.
     """
-    # NOTE Pins use "gpio numbering", not "physical numbering"! https://www.raspberrypi.org/documentation/usage/gpio-plus-and-raspi2/README.md
-    # e.g. 21 is GPIO21, not physical pin 21
     with MAX31865(config.gpio_sensor_cs,
                 config.gpio_sensor_miso,
                 config.gpio_sensor_mosi,
@@ -32,12 +30,11 @@ def main():
         print(temp.temperature())       
         while True:
             logging.info('Temperature: %0.2f°C', temp.temperature())
-            GPIO.setup(config.gpio_heat, GPIO.OUT)
             GPIO.output(config.gpio_heat, GPIO.HIGH)
             time.sleep(3)
             GPIO.output(config.gpio_heat, GPIO.LOW)
             time.sleep(1)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    #logging.basicConfig(level=logging.DEBUG)
     main()
