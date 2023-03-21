e2bf540b363310231f8412804f89450065a0ba8f
onehate <onehate@gmail.com>
2023-03-21 11:57:09 +0300

    Update oven.py

--- lib/oven.py
+++ lib/oven.py
@@ -443,7 +443,7 @@ class TempSensorReal(TempSensor):
             
         while True:
             try:
-                #print(self.thermocouple)
+                print(self.thermocouple)
                 self.temperature = self.thermocouple
                 lasttemp = self.temperature
             except Exception:
