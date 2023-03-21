4852498b71728d75c956a37d8e389e4b5fe5d56a
onehate <onehate@gmail.com>
2023-03-21 22:41:55 +0300

    jj

--- lib/example.py
+++ lib/example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8; python-indent-offset: 4; -*-
 
-
+import os
 import logging
 import time
 import sys
@@ -24,10 +24,10 @@ def main():
     """
     # NOTE Pins use "gpio numbering", not "physical numbering"! https://www.raspberrypi.org/documentation/usage/gpio-plus-and-raspi2/README.md
     # e.g. 21 is GPIO21, not physical pin 21
-    with MAX31865(config2.gpio_sensor_cs,
-                config2.gpio_sensor_miso,
-                config2.gpio_sensor_mosi,
-                config2.gpio_sensor_clock
+    with MAX31865(config.gpio_sensor_cs,
+                config.gpio_sensor_miso,
+                config.gpio_sensor_mosi,
+                config.gpio_sensor_clock
     ) as temp:
         print(temp.temperature())       
         while True:
