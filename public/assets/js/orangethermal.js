eb9d7fd8a26a3522174a79a32c5cd971b3e0d14f
onehate <onehate@gmail.com>
2023-03-24 23:19:41 +0300

    kk

--- public/assets/js/orangethermal.js
+++ public/assets/js/orangethermal.js
@@ -611,24 +611,7 @@ $(document).ready(function()
                     }
                 }
 
-                if(state=="RUNNING")
-                {
-                    $("#nav_start").hide();
-                    $("#nav_stop").show();
-
-                    graph.live.data.push([adjruntime, x.temperature]);
-                    graph.target.data.push(); //[adjruntime, x.target]
-                    graph.plot = $.plot("#graph_container", [ graph.profile, graph.target, graph.live ] , getOptions());
-
-                    left = parseInt(x.totaltime-x.runtime);
-                    eta = new Date(left * 1000).toISOString().substr(11, 8);
-
-                    updateProgress(parseFloat(x.runtime)/parseFloat(x.totaltime)*100);
-                    $('#state').html('<span class="glyphicon glyphicon-time" style="font-size: 22px; font-weight: normal"></span><span style="font-family: Digi; font-size: 40px;">' + eta + '</span>');
-                    $('#target_temp').html(parseInt(x.target));
-                    $('#heat_level').html(parseInt(x.heat * 100));
-                }
-                if(state == "TUNING")
+                if(state=="RUNNING" || state == "TUNING")
                 {
                     $("#nav_start").hide();
                     $("#nav_stop").show();
